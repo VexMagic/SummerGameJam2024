076c3bda82a456911b5eedcddf76bef160939387
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2024-06-02 12:20:21 +0200

    Revert "Merge branch 'ParticleSystemWindStuff'"
    This reverts commit 78c2fb3bd01a13b14db74c8f2925c8d50b1885ff, reversing
    changes made to e3f8e9c706952b55fee1c991f2b03ec907982d5f.

--- GameJam2024/Assets/Script/WeatherEffects.cs
+++ GameJam2024/Assets/Script/WeatherEffects.cs
@@ -7,32 +7,23 @@ public class WeatherEffects : MonoBehaviour
 {
     public enum WeatherEffect { Mosquitos, Neutral, Rainy, Windy};
 
-    public static WeatherEffect currentEffect;
+    public WeatherEffect currentEffect;
 
 
     public float weatherLength = 10f;
     //public GameObject playerObject;
     //public Transform playerTransform;
-    //public SpriteRenderer playerSprite;
+    public SpriteRenderer playerSprite;
     public Rigidbody2D player;
     Vector2 windDirection = new Vector2(0, 0);
-    public float windStrength = 2.5f;
+    public float windStrength = 0.5f;
     private float resetReference;
-
-    [SerializeField]
-    private ParticleSystem mosquitosPrefab;
-
-    public GameObject particleSystemLeft;
-    public GameObject particleSystemRight;
-    public GameObject particleSystemRain;
-    //public Transform particleTransform;
+    
 
     void Start()
     {
         currentEffect = WeatherEffect.Neutral;
-        resetReference = weatherLength;
-
-        
+        resetReference = 10f;
     }
 
     // Update is called once per frame
@@ -63,25 +54,25 @@ public class WeatherEffects : MonoBehaviour
         switch(currentEffect)
         { 
             case WeatherEffect.Mosquitos:
-                //playerSprite.color = Color.red;
+                playerSprite.color = Color.red;
                 break;
             case WeatherEffect.Rainy:
-                //playerSprite.color = Color.blue;
+                playerSprite.color = Color.blue;
 
                 break;
             case WeatherEffect.Windy:
-                //playerSprite.color = Color.green;
+                playerSprite.color = Color.green;
                 if(player != null)
                 {
                     //playerTransform.x -= windDirection.x;
-                    player.AddForce(windDirection * windStrength);
+                    //player.AddForce(windDirection * windStrength);
                 }
                 
 
                 break;
             case WeatherEffect.Neutral:
 
-                //playerSprite.color = Color.white;
+                playerSprite.color = Color.white;
                 break;
         }
 
@@ -94,44 +85,27 @@ public class WeatherEffects : MonoBehaviour
         //If the weather type is not Neutral (aka, rainy, windy or mosquito) then it returns to neutral
         if(currentEffect != WeatherEffect.Neutral)
         {
-            if(particleSystemLeft.active == true || particleSystemRight.active == true)
-            {
-                particleSystemLeft.SetActive(false);
-                particleSystemRight.SetActive(false);
-            }
-
-            if(particleSystemRain == true)
-            {
-                particleSystemRain.SetActive(false);
-            }
-            
             currentEffect = WeatherEffect.Neutral;
             weatherLength = 10f;
             
         }
         else //If the weather is going from neutral then it chooses a random other type to switch to
         {
-            int randomInt = UnityEngine.Random.Range(0, 3);
+            int randomInt = UnityEngine.Random.Range(0, 2);
             if (randomInt == 0)
             {
                 RandomWindDirection();
-
                 currentEffect = WeatherEffect.Windy;
                 weatherLength = resetReference;
             }
             else if (randomInt == 1)
             {
                 currentEffect = WeatherEffect.Mosquitos;
-                ParticleSystem newMosquitos = Instantiate(mosquitosPrefab, transform); 
-                Mosquito newMosquito = newMosquitos.GetComponent<Mosquito>();
-                newMosquito.playerTransform = player.gameObject.transform;
-
                 weatherLength = resetReference;
                 
             }
             else if(randomInt == 2)
             {
-                particleSystemRain.SetActive(true);
                 currentEffect = WeatherEffect.Rainy;
                 weatherLength = resetReference;
                 
@@ -143,23 +117,18 @@ public class WeatherEffects : MonoBehaviour
     //Choose a random direction for the wind
     void RandomWindDirection()
     {
-        int direction = UnityEngine.Random.Range(0, 2);
+        int direction = UnityEngine.Random.Range(0, 1);
         //int plusMinus = UnityEngine.Random.Range(0, 1);
 
         if (direction == 0)
         {
             windDirection = Vector2.left;
-            particleSystemLeft.SetActive (true);
-            //particleTransform.position = new Vector3(15,0,0);
-            //particleTransform.localRotation = Quaternion.Euler(-180, 0, 0);   
+
             //Push player in one direction
         }
         else
         {
             windDirection = Vector2.right;
-            particleSystemRight.SetActive (true);
-            //particleTransform.position = new Vector3(-15, 0, 0);
-            //particleTransform.localRotation = Quaternion.Euler(0, 0, 0);
             //Push player in other direction
         }
     }
@@ -169,7 +138,6 @@ public class WeatherEffects : MonoBehaviour
     //    yield return new WaitForSeconds(10);
     //}
 
-    
 
 }
 
